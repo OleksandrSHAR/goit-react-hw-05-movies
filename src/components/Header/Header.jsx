a0db6c382f0e605519d3a7c81bd206b89ce29017
OleksandrSHAR <125485600+OleksandrSHAR@users.noreply.github.com>
2023-10-06 14:26:57 +0300

    fin

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,6 +7,7 @@ export const Header = () => {
           width="154"
           height="20"
           src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+          alt="logo"
         />
       </NavLink>
       <NavLink to={'/'} end>
